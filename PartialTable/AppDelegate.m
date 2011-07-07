f26367d53f96fa03f139940bf3f1060b8e189a70
Abizer Nasir <abizern@abizern.org>
2011-07-07 15:27:42 +0100

    Add a helper method to return a random string.
    This will be used to create the data for the example.

--- PartialTable/AppDelegate.m
+++ PartialTable/AppDelegate.m
@@ -5,7 +5,11 @@
 //  Created by Abizer Nasir on 07/07/2011.
 //
 
+
+
 #import "AppDelegate.h"
+
+NSString *randomString();
 
 @implementation AppDelegate
 
@@ -14,6 +18,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
     [self.window makeKeyAndVisible];
     return YES;
 }
@@ -21,6 +26,23 @@
 - (void)dealloc {
     [_window release];
     [super dealloc];
+}
+
+// Mark: -
+// Mark: Convenience functions
+
+NSString *randomString() {
+    // Returns a random string made up of the first 13 characters of a UUID
+    CFUUIDRef uuidRef = CFUUIDCreate(kCFAllocatorDefault);
+    CFStringRef uuidStringRef = CFUUIDCreateString(kCFAllocatorDefault, uuidRef);
+        
+    NSRange aRange = NSMakeRange(0, 13); // Substring to the second hyphen
+    NSString *randomString = [(NSString *)uuidStringRef substringWithRange:aRange];
+    
+    CFRelease(uuidRef);
+    CFRelease(uuidStringRef);
+    
+    return randomString;
 }
 
 @end
