37e696f97678116ff795cbe191a07563dda0b9e7
Abizer Nasir <abizern@abizern.org>
2015-02-07 18:53:56 +0000

    Basic project and files recreated in Swift

---
+++ PartialTable/TableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TableViewController.swift
+//  PartialTable
+//
+//  Created by Abizer Nasir on 07/02/2015.
+//  Copyright (c) 2015 Jungle Candy Software Ltd. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+}
